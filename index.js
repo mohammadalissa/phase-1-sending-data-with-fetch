ce373354445055d44f03b4a9b9db76cb4e38fffd
Mohammad <mohammed.alissa627@gmail.com>
2024-02-02 21:34:23 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Add your code here
+function submitData() {
+    return (    fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json', // Correct syntax with single quotes around 'Content-Type'
+            'Accept': 'application/json', // Also make sure 'Accept' is in single quotes
+        },
+        body: JSON.stringify({
+            name: "Steve",
+            email: 'steve@steve.com',
+        }),
+    })
+    .then(response => response.json())
+    .then(data => {
+        // Access the converted object (parsed JSON)
+        console.log('Parsed Data:', data);
+
+        // Find the new id from the response data
+        const newId = data.id;
+
+        // Append the new id to the DOM
+        appendIdToDOM(newId);
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    }));
+}
